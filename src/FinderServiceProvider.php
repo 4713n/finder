1c44dfe5c633b26f6f0ded64ab0c9ac4e17e92aa
Alexander Landori <alexander.landori@creativesites.sk>
2024-01-19 16:08:12 +0100

    added testbench + setup package auto-discovery

---
+++ src/FinderServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace link0\Finder;
+
+use Illuminate\Support\ServiceProvider;
+
+class FinderServiceProvider extends ServiceProvider {
+	public function register() {
+		
+	}
+
+	public function boot() {
+
+    }
+}
